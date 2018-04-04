f6f69d962c96b59947b4d0bf8b94e3093c568c2a
Matt Siwiec <matt.siwiec@sendgrid.com>
2018-04-04 12:45:30 -0600

    add eslint plugin for cypress

---
+++ cypress/integration/testing_spec.js
@@ -0,0 +1,9 @@
+describe("My First Test", function() {
+  it("Does not do much!", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("Does more than you know!", function() {
+    expect(true).to.equal(false);
+  });
+});
