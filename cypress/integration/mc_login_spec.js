510c94f0750711a16d970334617404cc64bc1117
Matt Siwiec <matt.siwiec@sendgrid.com>
2018-05-01 10:40:18 -0600

    login w/o ui

--- cypress/integration/mc_login_spec.js
+++ cypress/integration/mc_login_spec.js
@@ -1,32 +1,53 @@
 const config = require('../../lib/envConfig');
 
-describe("My First Test", function() {
-  it("Does not do much!", function() {
-    expect(true).to.equal(true);
-  });
+describe("My First Test", () => {
+  let token;
 
-  it("Does more than you know!", function() {
-    expect(true).to.equal(false);
+  before(function(){
+    cy.request(
+        "POST",
+        "https://api.testing.sendgrid.com/v3/public/tokens",
+        {
+          username: "nlvx_automated_test_user_paid_26",
+          password: "T3stPasS"
+        }
+      )
+      .then(response => {
+        debugger;
+        cy.log(response.body.token);
+        expect(response.status).to.eq(200);
+        token = response.body.token;
+      });
   });
+  // it("Does not do much!", () => {
+  //   expect(true).to.equal(true);
+  // });
 
-  it("logs into sendgrid and navigates to marketing campaigns", function () {
+  // it("Does more than you know!", () => {
+  //   expect(true).to.equal(false);
+  // });
+
+  it("logs into sendgrid and navigates to marketing campaigns",  () => {
     cy.visit(config.url)
 
-    // Get an input, type into it and verify that the value has been updated
-    // username
-    cy.get('#usernameContainer-input-id')
-      .type('nlvx_automated_test_user_paid_26')
-      .should('have.value', 'nlvx_automated_test_user_paid_26')
+    // // Get an input, type into it and verify that the value has been updated
+    // // username
+    // cy.get('#usernameContainer-input-id')
+    //   .type('nlvx_automated_test_user_paid_26')
+    //   .should('have.value', 'nlvx_automated_test_user_paid_26')
 
-    // password
-    cy.get('#passwordContainer-input-id')
-      .type('T3stPasS')
+    // // password
+    // cy.get('#passwordContainer-input-id')
+    //   .type('T3stPasS')
 
-    // login button clicked
-    cy.get("[data-role=login-btn]").click()
+    // // login button clicked
+    // cy.get("[data-role=login-btn]").click()
+
+    cy.log("I don't work with logging/debugging"); // in cli
 
     // open nav dropdown
     cy.get('[data-nav-title=marketing]').click();
+    cy.debug();
 
     // click on Marketing campaigns link
     cy.contains('Marketing Campaigns').click();
